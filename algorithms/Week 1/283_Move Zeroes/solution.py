1e790dcde7988c7f0a9fa2bea9faeb22d7fb66f2
xinle1030 <xinlelam1030@gmail.com>
2023-11-23 17:00:07 +0800

    add a second solution to leetcode 283

--- algorithms/Week 1/283_Move Zeroes/solution.py
+++ algorithms/Week 1/283_Move Zeroes/solution.py
@@ -15,10 +15,28 @@ class Solution:
         while j < len(nums):
             nums[j] = 0
             j += 1
+    
+    def moveZeroes2(self, nums):
+        # move non zero element to the left and zero to the right 
+        temp = -1
+        i = 0
+
+        while i < len(nums):
+            if temp == -1 and nums[i] == 0:
+                temp = i
+            elif temp != -1 and nums[i] != 0:
+                nums[temp], nums[i] = nums[i], 0
+                i = temp
+                temp = -1
+            i += 1
 
 
 mySol = Solution()
-arr = [0,1,0,3,3,12]
-mySol.moveZeroes(arr)
-print(arr)
+# arr = [0,1,0,3,3,12]
+# mySol.moveZeroes(arr)
+# print(arr)
+
+arr2 = [0,1,0,3,3,12]
+mySol.moveZeroes2(arr2)
+print(arr2)
 
