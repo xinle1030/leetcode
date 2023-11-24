d1d5477732666f8fe3d2a81eeea56dce1b3ca3a3
xinle1030 <xinlelam1030@gmail.com>
2023-11-24 18:19:46 +0800

    complete iterative method

--- algorithms/Week 1/D_206_Reverse Linked List/solution.py
+++ algorithms/Week 1/D_206_Reverse Linked List/solution.py
@@ -1,6 +1,39 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
-    pass
 
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        current = head # 1
 
+        while current is not None:
+            forward = current.next # 2
+            current.next = prev # 1 -> None
+            prev = current
+            current = forward
+        
+        return prev
+    
+listNode5 = ListNode(5)
+listNode4 = ListNode(4)
+listNode3 = ListNode(3)
+listNode2 = ListNode(2)
+listNode1 = ListNode(1)
+
+listNode1.next = listNode2
+listNode2.next = listNode3
+listNode3.next = listNode4
+listNode4.next = listNode5
+
+head = listNode1
 mySol = Solution()
-
+ret = mySol.reverseList(head)
+while ret is not None:
+    print(ret.val)
+    ret = ret.next
