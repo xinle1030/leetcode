fdeb47f3608c3cbf1f640e9c98776959365125df
xinle1030 <xinlelam1030@gmail.com>
2023-11-27 20:13:04 +0800

    complete leetcode 155

--- algorithms/Week 1/H_155_Min Stack/solution.py
+++ algorithms/Week 1/H_155_Min Stack/solution.py
@@ -1,6 +1,35 @@
-class Solution:
-    pass
+class MinStack:
 
+    class Node:
+        def __init__(self, val: int, min: int, next):
+            self.val = val
+            self.min = min
+            self.next = next
 
-mySol = Solution()
+    def __init__(self):
+        self.head = None
 
+    def push(self, val: int) -> None:
+        if self.head == None:
+            self.head = self.Node(val, val, None)
+        else:
+            self.head = self.Node(val, min(self.head.min, val), self.head)
+
+    def pop(self) -> None:
+        self.head = self.head.next
+
+    def top(self) -> int:
+        return self.head.val
+
+    def getMin(self) -> int:
+        return self.head.min
+
+# Your MinStack object will be instantiated and called as such:
+minStack = MinStack()
+minStack.push(-2)
+minStack.push(0)
+minStack.push(-3)
+print(minStack.getMin()) # return -3
+minStack.pop()
+print(minStack.top())    # return 0
+print(minStack.getMin()) # return -2
