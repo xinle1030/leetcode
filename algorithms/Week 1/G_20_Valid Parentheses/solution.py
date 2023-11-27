f89a1163d986579da39810292aa449a323978db9
xinle1030 <xinlelam1030@gmail.com>
2023-11-27 19:32:35 +0800

    complete leetcode 20

--- algorithms/Week 1/G_20_Valid Parentheses/solution.py
+++ algorithms/Week 1/G_20_Valid Parentheses/solution.py
@@ -1,6 +1,39 @@
-class Solution:
-    pass
+from collections import deque
 
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = deque()
+        brackets = {
+            '(': ')',
+            '{': '}',
+            '[': ']'
+        }
+
+        for ch in s:
+            # current string is open bracket
+            if ch in brackets.keys():
+                stack.append(ch)
+            # current string is closed bracket which needs to be checked
+            else:
+                if len(stack) > 0:
+                    item = stack.pop()
+                    closeBracket = brackets[item]
+                else:
+                    closeBracket = "#"
+                if ch != closeBracket:
+                    return False
+        
+        return len(stack) == 0
 
 mySol = Solution()
+s = "()"
+print(mySol.isValid(s))
 
+s = "()[]{}"
+print(mySol.isValid(s))
+
+s = "(]"
+print(mySol.isValid(s))
+
+s = "]"
+print(mySol.isValid(s))
