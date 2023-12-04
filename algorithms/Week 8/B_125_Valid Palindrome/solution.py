9a5b4ad120204d28e700308f6423c22294a66bf7
xinle1030 <xinlelam1030@gmail.com>
2023-12-04 18:40:34 +0800

    complete leetcode 125

--- algorithms/Week 8/B_125_Valid Palindrome/solution.py
+++ algorithms/Week 8/B_125_Valid Palindrome/solution.py
@@ -1,42 +1,39 @@
 from typing import List
 
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-
-        j = 0
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[j] = nums[i]
-                j += 1
-        
-        while j < len(nums):
-            nums[j] = 0
-            j += 1
-    
-    def moveZeroes2(self, nums):
-        # move non zero element to the left and zero to the right 
-        temp = -1
+    def isPalindrome(self, s: str) -> bool:
         i = 0
+        j = len(s) - 1
 
-        while i < len(nums):
-            if temp == -1 and nums[i] == 0:
-                temp = i
-            elif temp != -1 and nums[i] != 0:
-                nums[temp], nums[i] = nums[i], 0
-                i = temp
-                temp = -1
-            i += 1
+        while i < j:
+            if not s[i].isalnum():
+                i += 1
+            elif not s[j].isalnum():
+                j -= 1
+            elif s[i].lower() == s[j].lower():
+                i += 1
+                j -= 1
+            else:
+                return False
 
+        return True
 
 mySol = Solution()
-# arr = [0,1,0,3,3,12]
-# mySol.moveZeroes(arr)
-# print(arr)
 
-arr2 = [0,1,0,3,3,12]
-mySol.moveZeroes2(arr2)
-print(arr2)
+s = "A man, a plan, a canal: Panama"
+print(mySol.isPalindrome(s))
 
+s = "race a car"
+print(mySol.isPalindrome(s))
+
+s = " "
+print(mySol.isPalindrome(s))
+
+s = "0P"
+print(mySol.isPalindrome(s))
+
+s = "a."
+print(mySol.isPalindrome(s))
+
+s = ".,"
+print(mySol.isPalindrome(s))
