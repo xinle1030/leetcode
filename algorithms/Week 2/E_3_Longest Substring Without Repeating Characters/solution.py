652112fd0c32c8af7b8b629c55de199a7b150091
xinle1030 <xinlelam1030@gmail.com>
2023-12-04 20:28:29 +0800

    complete leetcode 3

--- algorithms/Week 2/E_3_Longest Substring Without Repeating Characters/solution.py
+++ algorithms/Week 2/E_3_Longest Substring Without Repeating Characters/solution.py
@@ -1,6 +1,35 @@
 class Solution:
-    pass
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        i = 0 # start
+        j = 0 # move pointer
+        maxLen = 0
+        charIndices = [-1] * 128
 
+        for j in range(len(s)):
+            # check if the current character has occurred within the current substring by comparing its index in charIndex with i.
+            if charIndices[ord(s[j])] >= i:
+                # move i to the index after the occurrence of repeated character
+                i = charIndices[ord(s[j])] + 1
+            charIndices[ord(s[j])] = j
+
+            # length of the current window (right - left + 1)
+            maxLen = max(maxLen, j - i + 1)
+
+        return maxLen
 
 mySol = Solution()
 
+s = "abcabcbb"
+print(mySol.lengthOfLongestSubstring(s))
+
+s = "bbbbb"
+print(mySol.lengthOfLongestSubstring(s))
+
+s = "pwwkew"
+print(mySol.lengthOfLongestSubstring(s))
+
+s = " "
+print(mySol.lengthOfLongestSubstring(s))
+
+s = "dvdf"
+print(mySol.lengthOfLongestSubstring(s))
