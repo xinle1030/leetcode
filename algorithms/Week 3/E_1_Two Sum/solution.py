450f8f82047cfbc2ba24a1ab0327e69e2aac1626
xinle1030 <xinlelam1030@gmail.com>
2023-12-05 18:05:53 +0800

    complete leetcode 1

--- algorithms/Week 3/E_1_Two Sum/solution.py
+++ algorithms/Week 3/E_1_Two Sum/solution.py
@@ -1,6 +1,26 @@
-class Solution:
-    pass
+from typing import List
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement not in map:
+                map[num] = i
+            else:
+                return [map[complement], i]
+            
 
 mySol = Solution()
 
+nums = [2,7,11,15]
+target = 9
+print(mySol.twoSum(nums, target))
+
+nums = [3,2,4]
+target = 6
+print(mySol.twoSum(nums, target))
+
+nums = [3,3]
+target = 6
+print(mySol.twoSum(nums, target))
