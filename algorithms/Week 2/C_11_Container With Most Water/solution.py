939a5394fc310e39bace4306b2210a30aef2aadd
xinle1030 <xinlelam1030@gmail.com>
2024-01-06 17:42:47 +0800

    complete leetcode 11

--- algorithms/Week 2/C_11_Container With Most Water/solution.py
+++ algorithms/Week 2/C_11_Container With Most Water/solution.py
@@ -1,5 +1,29 @@
-class Solution:
-    pass
+from typing import List
 
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+
+        max_area = 0
+
+        while left < right:
+            min_height = min(height[left], height[right])
+            area = min_height * (right - left)
+
+            if area > max_area:
+                max_area = area
+            
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        
+        return max_area
 
 mySol = Solution()
+height = [1,8,6,2,5,4,8,3,7]
+print(mySol.maxArea(height))
+
+height = [1,1]
+print(mySol.maxArea(height))
