e6ce92242f643df98ba78c3b0d02e5d7fcc420e7
xinle1030 <xinlelam1030@gmail.com>
2024-01-15 19:47:50 +0800

    complete leetcode 560

--- algorithms/Week 2/G_560_Subarray Sum Equals K/solution.py
+++ algorithms/Week 2/G_560_Subarray Sum Equals K/solution.py
@@ -1,6 +1,44 @@
-class Solution:
-    pass
+from typing import List
+from collections import defaultdict
 
+"""
+Concept use prefix sum (cumulative sum)
+"""
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
+        currentSum = 0
+
+        prevSum = defaultdict(int)
+
+        for i in range(len(nums)):
+            currentSum += nums[i]
+
+            if currentSum == k:
+                count += 1
+            
+            # currsum exceeds given sum by currsum  - sum.
+            # Find number of subarrays having  
+            # this sum and exclude those subarrays 
+            # from currsum by increasing count by  
+            # same amount. 
+            if (currentSum - k) in prevSum:
+                count += prevSum[currentSum - k]
+            
+            # Add currsum value to count of  
+            # different values of sum. 
+            prevSum[currentSum] += 1
+
+        return count
 
 mySol = Solution()
+
+nums = [1,1,1]
+k = 2
+print(mySol.subarraySum(nums, k))
+
+nums = [1,2,3]
+k = 3
+print(mySol.subarraySum(nums, k))
 
