0ec42cd1145099a596390b40a1bbda2b00e3df91
xinle1030 <xinlelam1030@gmail.com>
2024-03-11 13:20:04 +0800

    complete leetcode 104

--- algorithms/Week 4/A_104_Maximum Depth of Binary Tree/solution.py
+++ algorithms/Week 4/A_104_Maximum Depth of Binary Tree/solution.py
@@ -1,6 +1,29 @@
-class Solution:
-    pass
+from typing import Optional
 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        maxDepthLeft = self.maxDepth(root.left)
+        maxDepthRight = self.maxDepth(root.right)
+
+        return max(maxDepthLeft, maxDepthRight) + 1
 
 mySol = Solution()
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.right.right = TreeNode(6)
+
+print(mySol.maxDepth(root))
 
