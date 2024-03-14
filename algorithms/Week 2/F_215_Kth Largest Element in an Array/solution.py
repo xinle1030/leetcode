7a25eefb622563aba0ae802f5b981f6a10c0ca92
xinle1030 <xinlelam1030@gmail.com>
2024-03-14 16:17:43 +0800

    add minHeap method to 703

--- algorithms/Week 2/F_215_Kth Largest Element in an Array/solution.py
+++ algorithms/Week 2/F_215_Kth Largest Element in an Array/solution.py
@@ -1,7 +1,20 @@
 from typing import List
 import random
+import heapq
 
 class Solution:
+    """
+    Method 1: Quick Select
+
+    Time Complexity: 
+    Best and Average Case: O(N)
+    Worst Case: O(N^2)
+    The average performance is linear. However, in the worst case (very rare, especially with randomized pivot), the algorithm can degrade to O(N^2).
+    
+    Space Complexity: O(1)
+    The space used is constant. The algorithm modifies the original list in place and doesn't utilize any significant additional data structures. 
+    The recursive stack calls (in the worst case) are also bounded by the depth of the list, making it O(log⁡ N), but this is typically considered as O(1) space complexity in QuickSelect.
+    """
     def findKthLargest(self, nums: List[int], k: int) -> int:
         low = 0
         high = len(nums)
@@ -39,16 +52,39 @@ class Solution:
         
         # low, ..., i
         return low, i
-            
+
+    """
+    Method 2: Min Heap
+    Time Complexity: O(n log⁡ k)
+    Each of the n elements is processed once. However, heap operations take O(log ⁡k) time, leading to an overall complexity of O(n log⁡ k).
+    
+    Space Complexity: O(k)
+    The solution uses a heap with a maximum of k elements.
+    """
+    def findKthLargest2(self, nums: List[int], k: int) -> int:
+        minHeap = nums[:k] # do this for space complexity = O(k)
+        heapq.heapify(minHeap)
+
+        for num in nums[k:]:
+            if num > minHeap[0]:
+                heapq.heappop(minHeap)
+                heapq.heappush(minHeap, num)
+        
+        return minHeap[0]
+ 
+
 mySol = Solution()
 nums = [3,2,1,5,6,4]
 k = 2
 print(mySol.findKthLargest(nums, k))
+print(mySol.findKthLargest2(nums, k))
 
 nums = [3,2,3,1,2,4,5,5,6]
 k = 4
 print(mySol.findKthLargest(nums, k))
+print(mySol.findKthLargest2(nums, k))
 
 nums = [1]
 k = 1
 print(mySol.findKthLargest(nums, k))
+print(mySol.findKthLargest2(nums, k))
