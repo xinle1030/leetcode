ace99daa6dcc1586194a013be6370c92d7fc6202
xinle1030 <xinlelam1030@gmail.com>
2024-03-19 16:42:41 +0800

    Complete leetcode 200

--- algorithms/Week 5/D_200_Number of Islands/solution.py
+++ algorithms/Week 5/D_200_Number of Islands/solution.py
@@ -1,6 +1,29 @@
+from typing import List
+
 class Solution:
-    pass
 
+    def numIslands(self, grid: List[List[str]]) -> int:
 
-mySol = Solution()
+        count = 0
 
+        if not grid:
+            return count
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    self.dfs(grid, i, j)
+                    count += 1
+        
+        return count
+                
+
+    
+    def dfs(self, grid, i, j):
+        if i < 0 or j < 0 or i >= len(grid) or j >= len((grid[i])) or grid[i][j] != "1":
+            return
+        grid[i][j] = "-"
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)
