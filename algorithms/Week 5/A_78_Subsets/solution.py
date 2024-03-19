0869be02b87a9be4cc957f493a6d762bc385e94a
xinle1030 <xinlelam1030@gmail.com>
2024-03-19 16:42:37 +0800

    update content

--- algorithms/Week 5/A_78_Subsets/solution.py
+++ algorithms/Week 5/A_78_Subsets/solution.py
@@ -1,15 +1,3 @@
-from typing import List
-
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        j = 0
-        for i in range(1, len(nums)):
-            if nums[i] != nums[j]:
-                # shift unique item to left 
-                j += 1  
-                nums[j] = nums[i]
-        return j + 1
-    
-mySol = Solution()
-arr = [1, 2, 2, 2, 3, 4, 4, 5, 5, 6]
-print(mySol.removeDuplicates(arr))
+"""
+5.3 Backtracking / A_78_Subsets.py
+"""
