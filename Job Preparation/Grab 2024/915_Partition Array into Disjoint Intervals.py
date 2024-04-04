f7d95c2542047787f93e8cf80acb6bdae2ee50b3
xinle1030 <xinlelam1030@gmail.com>
2024-04-04 13:31:31 +0800

    update notes

--- Job Preparation/Grab 2024/915_Partition Array into Disjoint Intervals.py
+++ Job Preparation/Grab 2024/915_Partition Array into Disjoint Intervals.py
@@ -15,6 +15,16 @@ class Solution:
         for i in range(N - 2, -1, -1):
             min_right[i] = min(min_right[i + 1], nums[i])
 
+        print(max_left)
+        print(min_right)
+        print()
+
+        """
+        Check for each index i if the maximum value from the left (max_left[i - 1]) is <= the minimum value from the right (min_right[i]). 
+        The first index i where this condition is met indicates the partition point.
+
+        The condition "max_left[i - 1] <= min_right[i]" ensures that the left subarray can be extended up to index i without violating the condition that every element in the left subarray is less than or equal to every element in the right subarray.
+        """
         for i in range(1, N):
             if max_left[i - 1] <= min_right[i]:
                 return i
